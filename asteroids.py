3b55f5ae3f8c6cdf43306c532db49a0667959786
uw-square <kristianchaves42@gmail.com>
2020-10-27 23:04:09 -0400

    Update asteroids.py
    asteroids version 2

--- asteroids.py
+++ asteroids.py
@@ -1,4 +1,5 @@
 from tkinter import PhotoImage, Canvas
+import random
 
 class asteroids:
     def __init__(self, x, y, canvas, speed=50):
@@ -7,9 +8,12 @@ class asteroids:
         self.__ypos = y
         self.__imagelist = []
         self.__speed = speed
-        self.__imagelist = [PhotoImage(file="images/asteroid0.png"), PhotoImage(file="images/asteroid1.png"), PhotoImage(file="images/asteroid2.png"), 
-            PhotoImage(file="images/explosion0.png"), PhotoImage(file="images/explosion1.png"), PhotoImage(file="images/explosion2.png")]
-        self.__asteroidlist = [0] * 21
+        self.__asteroidlist = [PhotoImage(file="images/asteroid0.png"), PhotoImage(file="images/asteroid1.png"), PhotoImage(file="images/asteroid2.png")] 
+        self.__explodeylist = [PhotoImage(file="images/explosion0.png"), PhotoImage(file="images/explosion1.png"), PhotoImage(file="images/explosion2.png")]
+        self.__size = random.randint(0,2)
+        self.__asteroidImage = self.__canvas.create_image(x,y,image=self.__asteroidlist[self.__size])
 
 
-    
+    def getLocation(self):
+        self.coordinates = [self.__xpos, self.__xpos + self.__asteroidlist[self.__size].width(), self.__ypos, self.__ypos + self.__asteroidlist[self.__size].height()]
+        return self.coordinates
