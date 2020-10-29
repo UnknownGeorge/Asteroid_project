78b1a68164d978192a6c52f8003232cc485e77ea
OranForever <33140521+OranForever@users.noreply.github.com>
2020-10-28 22:04:04 -0400

    Added new fonts

--- options.py
+++ options.py
@@ -12,20 +12,29 @@ class options:
         self.__bulletoption = 5
         self.Game = Game
         self.Health = health
+        self.background_list = [0] * 2
+        self.imgBackground = PhotoImage(file='images/space_background.png')
+        self.xpos = [0, self.imgBackground.width()]
+        self.canvas3 = Canvas(main_menu, width=self.imgBackground.width(), height=self.imgBackground.height())
+        self.canvas3.pack()
 
-        self.lblSpeed = Label(self.menu, width=60, font=('Arial', 14), anchor="c", text="Set the speed you want your ship to move at (max is 200):", bg="black", fg="white")
-        self.lblSpeed.place(x=self.menu.winfo_reqwidth() * 2 - self.lblSpeed.winfo_reqwidth() // 2 - 100, y=self.menu.winfo_reqheight() // 2)
-        self.entrySpeed = Entry(self.menu, width=12, font=('Arial', 10), justify='center', borderwidth=5, relief='flat')
-        self.entrySpeed.place(x=self.menu.winfo_reqwidth() * 2 + self.entrySpeed.winfo_reqwidth() * 2 + 40, y=self.menu.winfo_reqheight() // 2)
-        self.btnSetSpeed = Button(self.menu, width=12, font=('Arial', 10), justify='center', borderwidth=5, relief='flat', text="OK", command=lambda:self.setSpeedShip())
-        self.btnSetSpeed.place(x=self.menu.winfo_reqwidth() * 2 + self.entrySpeed.winfo_reqwidth() * 2 + 30, y=self.menu.winfo_reqheight() // 2 + 40)
-        self.lblHealth = Label(self.menu, width=60, font=('Arial', 14), anchor="c", text="Set the amount of health you have at each life (max is 10):", bg="black", fg="white")
-        self.lblHealth.place(x=self.menu.winfo_reqwidth() * 2 - self.lblSpeed.winfo_reqwidth() // 2 - 120, y=self.menu.winfo_reqheight() // 2 + 100)
-        self.entryHealth = Entry(self.menu, width=12, font=('Arial', 10), justify='center', borderwidth=5, relief='flat')
-        self.entryHealth.place(x=self.menu.winfo_reqwidth() * 2 + self.entryHealth.winfo_reqwidth() * 2 + 40, y=self.menu.winfo_reqheight() // 2 + 100)
-        self.btnSetHealth = Button(self.menu, width=12, font=('Arial', 10), justify='center', borderwidth=5, relief='flat', text="OK", command=lambda:self.setHealthShip())
-        self.btnSetHealth.place(x=self.menu.winfo_reqwidth() * 2 + self.entrySpeed.winfo_reqwidth() * 2 + 30, y=self.menu.winfo_reqheight() // 2 + 140)
-        self.btnBack = Button(self.menu, width= 12, font=('Arial', 10), justify='center', text="BACK",command=lambda:self.goBack(main_menu))
+        for i in range(len(self.background_list)):
+            self.background_list[i] = self.canvas3.create_image(self.xpos[i], 0, image=self.imgBackground, anchor='nw')
+        self.background_timer(self.menu)
+
+        self.lblSpeed = Label(self.menu, width=60, font=('neuropol', 14), anchor="c", text="Set the speed you want your \n ship to move at (max is 200):", bg="black", fg="white")
+        self.lblSpeed.place(x=self.menu.winfo_reqwidth() * 2 - self.lblSpeed.winfo_reqwidth() // 2 - 30, y=self.menu.winfo_reqheight() // 2)
+        self.entrySpeed = Entry(self.menu, width=12, font=('neuropol', 10), justify='center', borderwidth=5, relief='flat')
+        self.entrySpeed.place(x=self.menu.winfo_reqwidth() * 2 + self.entrySpeed.winfo_reqwidth() + 20, y=self.menu.winfo_reqheight() // 2)
+        self.btnSetSpeed = Button(self.menu, width=12, font=('neuropol', 10), justify='center', borderwidth=5, relief='flat', text="OK", command=lambda:self.setSpeedShip())
+        self.btnSetSpeed.place(x=self.menu.winfo_reqwidth() * 2 + self.entrySpeed.winfo_reqwidth() + 25, y=self.menu.winfo_reqheight() // 2 + 40)
+        self.lblHealth = Label(self.menu, width=60, font=('neuropol', 14), anchor="c", text="Set the amount of health you \n have at each life (max is 10):", bg="black", fg="white")
+        self.lblHealth.place(x=self.menu.winfo_reqwidth() * 2 - self.lblSpeed.winfo_reqwidth() // 2 - 30, y=self.menu.winfo_reqheight() // 2 + 100)
+        self.entryHealth = Entry(self.menu, width=12, font=('neuropol', 10), justify='center', borderwidth=5, relief='flat')
+        self.entryHealth.place(x=self.menu.winfo_reqwidth() * 2 + self.entryHealth.winfo_reqwidth() + 20, y=self.menu.winfo_reqheight() // 2 + 100)
+        self.btnSetHealth = Button(self.menu, width=12, font=('neuropol', 10), justify='center', borderwidth=5, relief='flat', text="OK", command=lambda:self.setHealthShip())
+        self.btnSetHealth.place(x=self.menu.winfo_reqwidth() * 2 + self.entrySpeed.winfo_reqwidth() + 25, y=self.menu.winfo_reqheight() // 2 + 140)
+        self.btnBack = Button(self.menu, width= 12, font=('neuropol', 10), justify='center', text="BACK",command=lambda:self.goBack(main_menu))
         self.btnBack.place(x=self.btnBack.winfo_reqwidth() // 2, y=self.menu.winfo_reqheight()*2)
 
 
@@ -47,4 +56,15 @@ class options:
         else:
             self.Game.setHealth(int(self.entryHealth.get()))
             messagebox.showinfo("Asteroid", "You have successfully changed your speed to: " + str(self.entryHealth.get()) + "!")
+    def background_timer(self, main_menu):
+        global btid
+        for i in range(len(self.background_list)):
+            self.canvas3.coords(self.background_list[i], self.xpos[i] - 5, 0)
+            self.xpos[i] -= 5
+        btid = main_menu.after(100, lambda: self.background_timer(main_menu))
+
+        if self.xpos[0] + self.imgBackground.width() <= 0:
+            self.xpos[0] = self.xpos[1] + self.imgBackground.width()
+        if self.xpos[1] + self.imgBackground.width() <= 0:
+            self.xpos[1] = self.xpos[0] + self.imgBackground.width()
 
