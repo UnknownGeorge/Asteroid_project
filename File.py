c6ee6c887f7d76fbf685f76fbcee2e2366e68370
george <georgesalib010@gmail.com>
2020-10-29 21:11:35 -0400

    File read and write

---
+++ File.py
@@ -0,0 +1,20 @@
+import json
+class top_file:
+    def __init__(self, file_location="scores.json"):
+        self.file_location = file_location
+
+    def writer(self, name, value):
+        with open(self.file_location,"r+") as files:
+            file_read = json.load(files)
+        with open(self.file_location,"w") as files:
+            file_read.update({len(file_read):{"name":name, "score":str(value)}})
+            #files.truncate(0)
+            json.dump(file_read, files)
+
+    def reader(self):
+        with open(self.file_location,"r+") as files:
+            file_read = json.load(files)
+            #sorted_list = sorted(file_read.items(), key=lambda x: x[1]["score"], reverse=True)
+            sorted_list = sorted(sorted(file_read.items(), key=lambda x:x[1]['score'], reverse=True), key=lambda x: x[1]['name'])
+            print(sorted_list)
+            return sorted_list
