ac0c4d47ce3aeb611444aa135d852f8d7c31760a
OranForever <33140521+OranForever@users.noreply.github.com>
2020-10-31 01:12:46 -0400

    updated file with comments

--- File.py
+++ File.py
@@ -1,24 +1,61 @@
 import json
 class top_file:
     def __init__(self, file_location="scores.json"):
+        '''
+        sets the file inside the project to the file location for use
+
+        PARAMETERS:
+        -----------
+        file_location: str
+            contains the json file to be used for reading and writing
+        '''
         self.file_location = file_location
 
     def writer(self, name, value):
+        '''
+        writes to the file
+
+        PARAMETERS:
+        -----------
+        file_location: str
+            contains the json file to be used for reading and writing
+        file_read: dictionary
+            contains all the information containing the scores and names
+        '''
         with open(self.file_location,"r+") as files:
             file_read = json.load(files)
         with open(self.file_location,"w") as files:
             file_read.update({len(file_read):{"name":name, "score":value}})
-            #files.truncate(0)
             json.dump(file_read, files)
 
     def reader(self):
+        '''
+        read from the file
+
+        PARAMETERS:
+        -----------
+        file_location: str
+            contains the json file to be used for reading and writing
+        file_read: dictionary
+            contains all the information containing the scores and names
+        sorted_list: list
+            contains the sorted list of the scores
+        '''
         with open(self.file_location,"r+") as files:
             file_read = json.load(files)
-            #sorted_list = sorted(file_read.items(), key=lambda x: x[1]["score"], reverse=True)
             sorted_list = sorted(sorted(file_read.items(), key=lambda x:x[1]['name'].upper()), key=lambda x: x[1]['score'],reverse=True)
-            #print(sorted_list)
             return sorted_list
     def top10(self):
+        '''
+        writes to the file
+
+        PARAMETERS:
+        -----------
+        values: list
+            contains the list of the top ten scores with names
+        reader: function
+            reads from the file
+        '''
         values = []
         if len(self.reader()) >= 10:
             for i in range(10):
