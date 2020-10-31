6b6570535c1f21aeeb961b56c7edb0c6277bbecd
uw-square <kristianchaves42@gmail.com>
2020-10-31 00:29:53 -0400

    Asteroids part 1
    snaidfogbiuodjspak

--- asteroids.py
+++ asteroids.py
@@ -4,24 +4,57 @@ import pygame
 
 class asteroids:
     def __init__(self, x, y, canvas, speed=10):
+        '''
+        Initializes elements of the asteroids class.
+        PARAMETERS:
+        -----------
+        speed: int
+            The speed at which the asteroid travels across the screen
+        asteroidlist: list
+            List of asteroid images
+        explodeylist: list 
+            List of asteroid explosions (only the smallest one is used. we thought that using all 
+            three for each decreasing level might be visually distracting to the user and make the user 
+            experience worse)
+        size: int
+            Determine the size of the asteroid
+        value: int
+            Determines the point value of destroying an asteroid
+        xpos: int
+            Determine the x position of the asteroid
+        ypos: int
+            Determine the y position of the asteroid
+        has_hurt: bool
+            Determines if the asteroid has been hit or not
+        happend: bool
+            Determines if the asteroid has been fully ended and is gone
+        can_be: bool
+            Detemines if the asteroid can be hit by the laser
+        '''
         self.__canvas = canvas
         self.__imagelist = []
         self.__speed = speed
-        self.__asteroidlist = [PhotoImage(file="images/asteroid0.png"), PhotoImage(file="images/asteroid1.png"), PhotoImage(file="images/asteroid2.png")]
-        self.__explodeylist = [PhotoImage(file="images/explosion0.png"), PhotoImage(file="images/explosion1.png"), PhotoImage(file="images/explosion2.png")]
+        self.__asteroidlist = [PhotoImage(file="images/asteroid0.png"), 
+            PhotoImage(file="images/asteroid1.png"), PhotoImage(file="images/asteroid2.png")]
+        self.__explodeylist = [PhotoImage(file="images/explosion0.png"), 
+            PhotoImage(file="images/explosion1.png"), PhotoImage(file="images/explosion2.png")]
         self.__size = random.randint(0,2)
-        self.value = (self.__size +1) *10
+        self.value = (self.__size + 1) * 10
         self.__xpos =  x
         self.__ypos = y
-        self.__mxpos = self.__xpos
-        self.__mypos = self.__ypos
-        self.__asteroidImage = self.__canvas.create_image(x,y,image=self.__asteroidlist[self.__size], anchor="nw")
+        self.__asteroidImage = self.__canvas.create_image(x,y,image = self.__asteroidlist[self.__size], anchor="nw")
         self.has_hurt = True
         self.happend = False
         self.can_be = True
-        #self.move()
 
     def set_pos(self, x = 0, y = 0):
+        '''
+        Sets the name of the student.
+        PARAMETERS:
+        -----------
+        name: str
+        The name of the student
+        '''
         if not x == 0:
             self.__xpos = x
         if not y == 0:
@@ -29,24 +62,29 @@ class asteroids:
         self.__mxpos = self.__xpos
         self.__mypos = self.__ypos
         self.__canvas.coords(self.__asteroidImage, self.__xpos, self.__ypos)
+    
     def getLocation(self):
-        self.coordinates = [self.__xpos, self.__xpos + self.__asteroidlist[self.__size].width(), self.__ypos, self.__ypos + self.__asteroidlist[self.__size].height()]
+        self.coordinates = [self.__xpos, self.__xpos + self.__asteroidlist[self.__size].width(), self.__ypos, 
+            self.__ypos + self.__asteroidlist[self.__size].height()]
         return self.coordinates
+    
     def move(self):
         self.__xpos -=self.__speed
         self.__canvas.coords(self.__asteroidImage, self.__xpos, self.__ypos)
+    
     def health(self, astroids_avalible, index):
-
         if not self.__size <= 0:
             self.__size -= 1
             self.__canvas.delete(self.__asteroidImage)
             self.__ypos =  self.__asteroidlist[self.__size].height() //2 +self.__ypos
-            self.__asteroidImage = self.__canvas.create_image( self.__xpos, self.__ypos,image=self.__asteroidlist[self.__size], anchor="nw")
+            self.__asteroidImage = self.__canvas.create_image( self.__xpos, self.__ypos, 
+                image = self.__asteroidlist[self.__size], anchor="nw")
 
         else:
             self.can_be = False
             self.__canvas.delete(self.__asteroidImage)
-            self.__asteroidImage = self.__canvas.create_image(self.__xpos,self.__ypos,image=self.__explodeylist[self.__size], anchor="nw")
+            self.__asteroidImage = self.__canvas.create_image(self.__xpos,self.__ypos, 
+                image = self.__explodeylist[self.__size], anchor="nw")
             self.__canvas.after(500, lambda: self.remake(astroids_avalible, index))
             # Cue Acursed Noises
             pygame.mixer.init()
@@ -59,26 +97,39 @@ class asteroids:
             pygame.mixer.Sound.play(self.explodySound[self.explodyNoise])
             return True
 
-
-
     def remake(self, astroids_avalible, index):
+        '''
+        Sets the name of the student.
+        PARAMETERS:
+        -----------
+        name: str
+        The name of the student
+        '''
         self.can_be = True
         self.has_hurt = True
         self.happend = False
         self.__canvas.delete(self.__asteroidImage)
         self.__size = random.randint(0,2)
-        self.value = (self.__size +1) *10
+        self.value = (self.__size +1) * 10
         rand_x, rand_y = random.randint(1100, len(astroids_avalible) * 160), random.randint(100, 400)
         self.__xpos = rand_x
         self.__ypos = rand_y
-        self.__asteroidImage = self.__canvas.create_image(rand_x, rand_y ,image=self.__asteroidlist[self.__size], anchor="nw")
+        self.__asteroidImage = self.__canvas.create_image(rand_x, rand_y , 
+            image = self.__asteroidlist[self.__size], anchor="nw")
         for i in range(60):
             for loc, z in enumerate(astroids_avalible):
                 if not loc == index:
-                    if ( z.getLocation()[1] >= self.getLocation()[0]   and z.getLocation()[0] <= self.getLocation()[1] or  z.getLocation()[0] <= self.getLocation()[0]   and z.getLocation()[1] >= self.getLocation()[1] ):
-                        if  z.getLocation()[3] >= self.getLocation()[2]  and z.getLocation()[2] <= self.getLocation()[3] or z.getLocation()[2]<= self.getLocation()[2]  and z.getLocation()[3] >= self.getLocation()[3]:
+                    if (z.getLocation()[1] >= self.getLocation()[0] and 
+                        z.getLocation()[0] <= self.getLocation()[1] or 
+                        z.getLocation()[0] <= self.getLocation()[0] and 
+                        z.getLocation()[1] >= self.getLocation()[1] ):
+                        if (z.getLocation()[3] >= self.getLocation()[2] and 
+                            z.getLocation()[2] <= self.getLocation()[3] or 
+                            z.getLocation()[2]<= self.getLocation()[2] and 
+                            z.getLocation()[3] >= self.getLocation()[3]):
                             rand_x, rand_y = random.randint(1100, len(astroids_avalible) * 160), random.randint(100, 400)
                             self.set_pos(x=rand_x, y = rand_y)
+
     def hurt_player(self):
         x = self.has_hurt
         self.has_hurt = False
@@ -90,12 +141,16 @@ class asteroids:
             return self.value
         else:
             return 0
+
     def set_speed(self, speed):
+        '''
+        Sets the speed of the asteroid
+        PARAMETERS:
+        -----------
+        speed: int
+        The speed of the asteroid
+        '''
         self.__speed =  speed
+
     def can_beam(self):
         return self.can_be
-
-        # self.__xpos = self.__mxpos
-        # self.__ypos = self.__mypos
-        # self.__size = random.randint(0,2)
-        # self.__asteroidImage = self.__canvas.create_image(self.__xpos,self.__ypos,image=self.__asteroidlist[self.__size], anchor="nw")
